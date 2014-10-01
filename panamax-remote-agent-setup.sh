bbc291011fae03951abae330d95aa6d6a5d1870a
Alex <me@alexwelch.com>
2014-10-01 08:18:33 -0600

    Update image references and a few other tweaks

--- panamax-remote-agent-setup.sh
+++ panamax-remote-agent-setup.sh
@@ -2,7 +2,7 @@
 SEP='|'
 KEY_NAME="pmx_remote_agent"
 CERT_DIR="/home/core/pmx_agent/certs"
-CERT_IMAGE="panamax/pmx_cert_generator"
+CERT_IMAGE="centurylink/openssl:latest"
 
 mkdir -p ${CERT_DIR}
 common_name=""
@@ -14,8 +14,6 @@ docker run --rm  -e COMMON_NAME=${common_name} -e KEY_NAME=${KEY_NAME} -v ${CERT
 
 url='${common_name}'
 PUBLIC_CERT="$(<${CERT_DIR}/${KEY_NAME}.crt)"
-
-echo "${PUBLIC_CERT}"
 
 if [[ ! -f ${CERT_DIR}/.env ]]; then
     PMX_AGENT_ID="`uuidgen`"
@@ -32,7 +30,7 @@ echo ""
 echo ""
 echo "Copy and paste the following to your panamax to connect to this remote instance:"
 echo "============================== START =============================="
-echo $common_name${SEP}${PMX_AGENT_ID}${SEP}${PMX_AGENT_PASSWORD}${SEP}${PUBLIC_CERT} | base64
+echo "https://${common_name}:3001${SEP}${PMX_AGENT_ID}${SEP}${PMX_AGENT_PASSWORD}${SEP}${PUBLIC_CERT}" | base64
 echo "============================== END =============================="
 
 #
@@ -42,7 +40,7 @@ echo "============================== END =============================="
 #docker build -t panamax/panamax-remote-agent .
 #
 adapter_dir="adapter"
-adapter_name="panamax/panamax-fleet-adapter"
+adapter_name="centurylink/panamax-fleet-adapter"
 adapter_container_name="pmx_adapter"
 #rm -rf ${adapter_dir}
 #git clone https://github.com/CenturyLinkLabs/fleet-adapter.git ${adapter_dir}
@@ -55,8 +53,8 @@ agent_container_name="pmx_agent"
 #git clone https://github.com/CenturyLinkLabs/panamax-remote-agent.git ${agent_dir}
 #cd ${agent_dir} && docker build -t ${agent_name} .
 
-echo docker run -d --name ${adapter_container_name} -v /var/run/docker.sock:/run/docker.sock --expose 4567 ${adapter_name}:latest
-echo docker run -d --name ${agent_container_name} --link ${adapter_container_name}:adapter -e REMOTE_AGENT_ID=${PMX_AGENT_ID} -e REMOTE_AGENT_API_KEY=${PMX_AGENT_PASSWORD}  -v ${CERT_DIR}:/usr/local/share -p 3001:3001 ${agent_name}:latest
+docker run -d --name ${adapter_container_name} -v /var/run/docker.sock:/run/docker.sock --expose 4567 ${adapter_name}:latest
+docker run -d --name ${agent_container_name} --link ${adapter_container_name}:adapter -e REMOTE_AGENT_ID=${PMX_AGENT_ID} -e REMOTE_AGENT_API_KEY=${PMX_AGENT_PASSWORD}  -v ${CERT_DIR}:/usr/local/share -p 3001:3000 ${agent_name}:latest
 
 
 #1. Gen the Cert
