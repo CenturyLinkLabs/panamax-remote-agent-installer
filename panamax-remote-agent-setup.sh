6da85f19e48c31275ddb82c0382c2d7c1498902e
Chaitanya Akkineni <cakkineni@gmail.com>
2014-10-29 16:14:52 -0500

    Updating remote agent/adapter installer

--- panamax-remote-agent-setup.sh
+++ panamax-remote-agent-setup.sh
@@ -26,7 +26,7 @@ echo_uninstall="delete:        Uninstalls Panamax remote agent/adapter."
 echo_help="help:          Show this help"
 echo_debug="debug:         Display your current Panamax settings."
 
-function displayLogo {
+function display_logo {
     tput clear
     echo ""
     echo -e "\033[0;31;32m███████╗ ██████╗  █████████╗ ██████╗ \033[0m\033[31;37m ██████████╗ ██████╗  ██╗  ██╗\033[0m"
@@ -37,18 +37,33 @@ function displayLogo {
     echo -e "\033[0;31;32m╚═╝      ╚══════╝ ╚══╝  ╚══╝ ╚══════╝\033[0m\033[31;37m ╚═╝ ╚═╝ ╚═╝ ╚══════╝ ╚═╝  ╚═╝\033[0m"
     echo ""
     echo -e "CenturyLink Labs - http://www.centurylinklabs.com/\n"
-
 }
 
 function cmd_exists() {
     while [ -n "$1" ]
     do
-        command -v "$1" >/dev/null 2>&1 || { echo >&2 " '$1' is required but not installed.  Aborting."; exit 1; }
+        if [[ "$1" == "docker" ]]; then
+            docker -v | grep -w '1\.[2-9]' 2>&1 >/dev/null
+            if [[ "$?" -ne "0" ]]; then
+                echo "docker 1.2 or later is required but not installed. Aborting."
+                exit 1;
+            fi
+        else
+            command -v "$1" >/dev/null 2>&1 || { echo >&2 " '$1' is required but not installed.  Aborting."; exit 1;}
+        fi
         shift
     done
 }
 
+function validate_install {
+    if [[ $(docker ps -a| grep "${ADAPTER_CONTAINER_NAME}\|${AGENT_CONTAINER_NAME}") == "" ]]; then
+        echo -e "\nYou don't have remote agent/adapter installed. Please install remote agent/adapter.\n\n"
+        exit 1;
+    fi
+}
+
 function uninstall {
+    validate_install
     echo -e "\nDeleting panamax remote agent/adapter containers..."
     docker rm -f ${AGENT_CONTAINER_NAME} > /dev/null 2>&1
     docker rm -f ${ADAPTER_CONTAINER_NAME} > /dev/null 2>&1
@@ -59,7 +74,8 @@ function uninstall {
     docker rmi ${AGENT_IMAGE}  > /dev/null 2>&1
 }
 
-function downloadImage {
+function download_image {
+    echo -e "\nDownloading docker image ${1}..."
     $(docker pull "${1}" > /dev/null 2>&1)&
     PID=$!
     while $(kill -n 0 "${PID}" 2> /dev/null)
@@ -70,17 +86,23 @@ function downloadImage {
     echo ""
 }
 
-function setEnvVar {
+function set_env_var {
     sed -i "/$1=/d" "$ENV"
     echo export $1=$2 >> "$ENV"
 }
 
-function setConfigVar {
+function set_adapter_config {
     sed -i "/$1=/d" "${ADAPTER_CONFIG}/.config"
-    echo export $1=$2 >> "${ADAPTER_CONFIG}/.config"
+    echo $1=$2 >> "${ADAPTER_CONFIG}/.config"
 }
 
-function installAdapter {
+function set_agent_config {
+    touch "${AGENT_CONFIG}/.config"
+    sed -i "/$1=/d" "${AGENT_CONFIG}/.config"
+    echo $1=$2 >> "${AGENT_CONFIG}/.config"
+}
+
+function install_adapter {
     if [[ $(docker ps -a| grep "${ADAPTER_CONTAINER_NAME}\|${AGENT_CONTAINER_NAME}") != "" ]]; then
         echo -e "\nYou already have remote agent/adapter installed. Please reinstall.\n\n"
         exit 1;
@@ -90,7 +112,7 @@ function installAdapter {
 
     mkdir -p ${ADAPTER_CONFIG}
 
-    echo -e "\nSelect the ochestrator you want to use: "
+    echo -e "\nSelect the ochestrator you want to use: \n"
     select operation in "Kubernetes" "CoreOS Fleet"; do
     case $operation in
         "Kubernetes") cluster_type=0; break;;
@@ -114,9 +136,9 @@ function installAdapter {
         read -p "Enter password for ${adapter_name} API:" api_password; echo
         stty echo
 
-        setConfigVar "${adapter_env_var_name}" "${api_url}"
-        setConfigVar API_USERNAME "${api_username}"
-        setConfigVar API_PASSWORD "${api_password}"
+        set_adapter_config PMX_ADAPTER_API_URL "${api_url}"
+        set_adapter_config PMX_ADAPTER_API_USERNAME "${api_username}"
+        set_adapter_config PMX_ADAPTER_API_PASSWORD "${api_password}"
     else
         adapter_image_name=${ADAPTER_IMAGE_FLEET}
         adapter_env_var_name="FLEETCTL_ENDPOINT"
@@ -125,18 +147,17 @@ function installAdapter {
           read -p "Enter the API endpoint to access the ${adapter_name} cluster (e.g: https://10.187.241.100:8080/): " api_url
         done
 
-        setConfigVar "${adapter_env_var_name}" "${api_url}"
+        set_adapter_config "${adapter_env_var_name}" "${api_url}"
     fi
 
-    setEnvVar "PMX_ADAPTER_IMAGE_NAME" \"${adapter_image_name}\"
+    set_adapter_config "PMX_ADAPTER_IMAGE_NAME" \"${adapter_image_name}\"
 
     echo -e "\nStarting Panamax ${adapter_name} adapter:"
-    downloadImage ${adapter_image_name}
+    download_image ${adapter_image_name}
     docker run -d --name ${ADAPTER_CONTAINER_NAME} -e ${adapter_env_var_name}="${api_url}" -e API_PASSWORD=${api_password} -e API_USERNAME=${api_username}  -v ${ADAPTER_CONFIG}:/usr/local/share/config --restart=always ${adapter_image_name}
 }
 
-
-function installAgent {
+function install_agent {
     echo -e "\nInstalling Panamax remote agent:"
 
     mkdir -p ${AGENT_CONFIG}
@@ -145,25 +166,27 @@ function installAgent {
       read -p "Enter the public hostname (dev.example.com) or IP Address (10.3.4.5) of the agent: " common_name
     done
 
-    read -p "Enter the port to run the agent on (${HOST_PORT}): " -i ${HOST_PORT} host_port
+    read -p "Enter the port to run the agent on (${HOST_PORT}): " host_port
+    host_port=${HOST_PORT:host_port}
 
     echo -e "\nGenerating SSL Key"
-    downloadImage ${CERT_IMAGE}
+    download_image ${CERT_IMAGE}
     docker run --rm  -e COMMON_NAME="${common_name}" -e KEY_NAME="${KEY_NAME}" -v "${AGENT_CONFIG}":/certs "${CERT_IMAGE}" > /dev/null 2>&1
 
     PUBLIC_CERT="$(<${AGENT_CONFIG}/${KEY_NAME}.crt)"
-    PMX_AGENT_ID="$(uuidgen)"
-    PMX_AGENT_PASSWORD="$(uuidgen | base64)"
-    setEnvVar PMX_AGENT_ID \""${PMX_AGENT_ID}"\"
-    setEnvVar PMX_AGENT_PASSWORD \""${PMX_AGENT_PASSWORD}"\"
+    set_agent_config PMX_AGENT_COMMON_NAME \""${common_name}"\"
+    set_agent_config PMX_AGENT_HOST_PORT \""${host_port}"\"
+    set_agent_config PMX_AGENT_AGENT_ID \""$(uuidgen)"\"
+    set_agent_config PMX_AGENT_AGENT_PASSWORD \""$(uuidgen | base64)"\"
 
     echo -e "\nStarting Panamax remote agent:"
-    downloadImage ${AGENT_IMAGE}
+    download_image ${AGENT_IMAGE}
     docker run -d --name ${AGENT_CONTAINER_NAME} --link ${ADAPTER_CONTAINER_NAME}:adapter -e REMOTE_AGENT_ID="${PMX_AGENT_ID}" -e REMOTE_AGENT_API_KEY="${PMX_AGENT_PASSWORD}"  --restart=always -v ${AGENT_CONFIG}:/usr/local/share/certs -p ${host_port}:3000 ${AGENT_IMAGE}
 
+    echo "https://${common_name}:${host_port}${SEP}${PMX_AGENT_ID}${SEP}${PMX_AGENT_PASSWORD}${SEP}${PUBLIC_CERT}" | base64  > ${AGENT_CONFIG}/panamax_agent_key
     echo ""
     echo "============================== START =============================="
-    echo "https://${common_name}:${host_port}${SEP}${PMX_AGENT_ID}${SEP}${PMX_AGENT_PASSWORD}${SEP}${PUBLIC_CERT}" | base64
+    cat ${AGENT_CONFIG}/panamax_agent_key
     echo "============================== END =============================="
     echo -e "\n\nCopy and paste the above (Not including start/end tags) to your local panamax client to connect to this remote agent."
     #echo -e "\n\n*** Add any additional configuration settings needed for the ${adapter_name} adapter to the following file: ${ADAPTER_CONFIG}/.config"
@@ -171,30 +194,51 @@ function installAgent {
 }
 
 function install {
-    installAdapter
-    installAgent
+    install_adapter
+    install_agent
 }
 
 function restart {
     echo "Restarting Panamax remote agent/adapter containers..."
+    validate_install
     docker restart ${ADAPTER_CONTAINER_NAME}
     docker restart ${AGENT_CONTAINER_NAME}
 }
 
 function update {
     echo "Updating Panamax remote agent/adapter images..."
-    downloadImage "${PMX_ADAPTER_IMAGE_NAME}"
-    downloadImage "${AGENT_IMAGE}"
-
+    validate_install
+    download_image "${PMX_ADAPTER_IMAGE_NAME}"
+    download_image "${AGENT_IMAGE}"
     restart
 }
 
 function reinstall {
+    echo -e "\nReinstall panamax remote agent/adapter"
+    validate_install
     uninstall
     install
 }
 
-function readParams {
+function debug {
+    echo ""
+    echo "============================== START =============================="
+    cat ${AGENT_CONFIG}/panamax_agent_key
+    echo "============================== END =============================="
+    echo -e "\n\nCopy and paste the above (Not including start/end tags) to your local panamax client to connect to this remote agent."
+}
+
+function show_long_help {
+    echo "Not Implemented."
+    exit 1;
+}
+
+function show_help {
+    echo "Not Implemented"
+    exit 1;
+}
+
+function read_params {
     for i in "$@"
     do
     case $(echo "$i" | tr '[:upper:]' '[:lower:]') in
@@ -223,11 +267,11 @@ function readParams {
         operation=debug
         ;;
         --help|-h|help)
-        showLongHelp;
+        show_long_help;
         exit 1;
         ;;
         *)
-        showLongHelp;
+        show_long_help;
         exit 1;
         ;;
     esac
@@ -235,7 +279,7 @@ function readParams {
 }
 
 function main {
-    displayLogo
+    display_logo
 
     if [ $UID -ne 0 ] ; then
         echo -e "\nPlease execute the installer as root.\n\n"
@@ -244,16 +288,18 @@ function main {
 
     cmd_exists curl uuidgen base64 docker
 
-    readParams "$@"
+    read_params "$@"
 
-    if [[ ! -f "${ENV}" ]]; then
-        source "${ENV}"
-    fi
+    mkdir -p ${ADAPTER_CONFIG}
+    mkdir -p ${AGENT_CONFIG}
+    touch "${ENV}"
+    touch "${ADAPTER_CONFIG}/.config"
+    touch "${AGENT_CONFIG}/.config"
 
     if [[ $# -gt 0 ]]; then
         case $operation in
-            install)   install "$@" || { showHelp; exit 1; } ;;
-            reinstall)   reinstall "$@" || { showHelp; exit 1; } ;;
+            install)   install "$@" || { show_help; exit 1; } ;;
+            reinstall)   reinstall "$@" || { show_help; exit 1; } ;;
             restart) restart;;
             check) echo "Not Implemented";;
             info) echo "Not Implemented";;
@@ -273,7 +319,7 @@ function main {
             "$echo_update")  echo "Not Implemented"; break;;
             "$echo_uninstall") uninstall; break;;
             "$echo_help") echo "Not Implemented"; break;;
-            "$echo_debug")  echo "Not Implemented"; break;;
+            "$echo_debug")  debug; break;;
             quit) exit 0;;
         esac
         done
